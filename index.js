d6d1cdbedf09ab80c7d3d35bcd4dedf4f8949c08
licunxing <864255598@qq.com>
2023-12-03 21:49:24 +0800

    feat: console tips, auto clone or pull

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const program = require("commander");
 const Koa = require("koa");
 const Router = require("koa-router");
 const { createProjectAction } = require("./lib/core/action");
+const initProject = require("./lib/core/initProject");
+
+require("dotenv").config();
 
 const app = new Koa();
 var router = new Router();
@@ -10,14 +12,15 @@ router.get("/deploy", async (ctx, next) => {
   await createProjectAction();
   ctx.body = "GET Success!";
 });
+
 router.post("/deploy", async (ctx, next) => {
   await createProjectAction();
   ctx.body = "POST Success!";
 });
-// app.use(async (ctx) => {
-//   await createProjectAction();
-//   ctx.body = "Success!";
-// });
+
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000);
+app.listen(3000, () => {
+  initProject();
+  console.log("Service started successfully: ", 3000);
+});
