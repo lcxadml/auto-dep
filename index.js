b73d49a3c997b2fa1e7848cbeab11bc8f3d1b81f
licunxing <864255598@qq.com>
2023-12-03 18:19:02 +0800

    feat: add router

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const program = require("commander");
 const Koa = require("koa");
+const Router = require("koa-router");
 const { createProjectAction } = require("./lib/core/action");
 
 const app = new Koa();
-app.use(async (ctx) => {
+var router = new Router();
+
+router.get("/deploy", async (ctx, next) => {
   await createProjectAction();
-  ctx.body = "Success!";
+  ctx.body = "GET Success!";
 });
+router.post("/deploy", async (ctx, next) => {
+  await createProjectAction();
+  ctx.body = "POST Success!";
+});
+// app.use(async (ctx) => {
+//   await createProjectAction();
+//   ctx.body = "Success!";
+// });
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
